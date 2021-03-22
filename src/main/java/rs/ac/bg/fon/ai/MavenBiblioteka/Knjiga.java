3cfdd7b357b6f0d1b6e5ff1637f54a38a4149f4a
ivanaastankovic <stankovic.ivana1998@gmail.com>
2021-03-22 15:11:09 +0100

    Napravljen projekat MavenBiblioteka.

---
+++ src/main/java/rs/ac/bg/fon/ai/MavenBiblioteka/Knjiga.java
@@ -0,0 +1,17 @@
+package rs.ac.bg.fon.ai.MavenBiblioteka;
+
+public class Knjiga {
+
+	public String naslov;
+
+	public String getNaslov() {
+		return naslov;
+	}
+
+	public void setNaslov(String naslov) {
+		if(naslov==null)
+			throw new NullPointerException("Naslov ne sme biti null");
+		this.naslov = naslov;
+	}
+	
+}
